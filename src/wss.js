ce957685017153d27e434fde3e9f15a76eca35be
jhenage <jhenage@gmail.com>
2022-03-01 15:01:56 -0700

    handle sessions

--- src/wss.js
+++ src/wss.js
@@ -8,7 +8,54 @@ export default class WSS {
         this.wss.on('connection', (ws) => {
             ws.on('message', async (message) => {
                 let request = JSON.parse(message);
-                
+
+                if(!request.session) {
+                    return;
+                }
+
+                if(!this.sessions[request.session]) {
+                    this.sessions[request.session] = [];
+                }
+
+                const session = this.sessions[request.session];
+                let me = session.indexOf(ws);
+                if(!session.includes(ws)) {
+                    session.push(ws);
+                    session.forEach(peer => {
+                        peer.send(JSON.stringify({
+                            size: session.length
+                        }));
+                    })
+                }
+
+                if(request.message) {
+                    session.forEach(peer => {
+                        if(peer != ws) {
+                            peer.send(JSON.stringify({
+                                message: request.message
+                            }));
+                        }
+                    })
+                }
+            });
+
+            ws.on('close',() => {
+                for(let i in this.sessions) {
+                    const session = this.sessions[i];
+                    if(session.includes(ws)) {
+                        session.splice(session.findIndex(ws),1);
+
+                        if(session.length) {
+                            session.forEach(peer => {
+                                peer.send(JSON.stringify({
+                                    size: session.length
+                                }));
+                            })
+                        } else {
+                            delete this.sessions[i];
+                        }
+                    }
+                }
             });
         })
     }
