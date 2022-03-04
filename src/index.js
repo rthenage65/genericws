968900e0766ad80eaea572ef3c9611c81ac4a840
jhenage <jhenage@gmail.com>
2022-03-04 10:24:58 -0700

    ssl infrastructure

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const WebSocket = require("ws");
 const { createServer } = require("http");
 const { WSS } = require("./wss");
 
+const fs = require('fs');
+const options = {
+//    key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+//    cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+}
+
 const port = 3000;
 const hostname = "0.0.0.0";
 const app = express();
-const httpServer = createServer(app);
+const httpServer = createServer(options,app);
 const wsserver = new WebSocket.Server({ server: httpServer });
 new WSS(wsserver);
 
